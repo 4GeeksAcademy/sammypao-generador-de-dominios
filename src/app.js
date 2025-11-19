b2fc77380572fca0547e063d4c052c5507f62c44
Shammy Murillo <75450484+sammypao@users.noreply.github.com>
2025-11-19 22:30:00 +0000

    primera parte terminada

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  
+  
+  for(let i = 0; i < pronoun.length; i++) {
+    
+    
+    for(let j = 0; j < adj.length; j++) {
+      
+      
+      for(let k = 0; k < noun.length; k++) {
+        
+        
+        let domain = pronoun[i] + adj[j] + noun[k] + ".com" + ".es" + ".net";
+        console.log(domain);
+      }
+    }
+  }
 };
+
