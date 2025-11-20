1ef4a809b11c6415ad5b64de51cdb53f2cb4983a
Shammy Murillo <75450484+sammypao@users.noreply.github.com>
2025-11-20 13:09:41 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -5,26 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  
+window.onload = function () {
+
   const pronoun = ['the', 'our'];
   const adj = ['great', 'big'];
   const noun = ['jogger', 'racoon'];
-  
-  
-  for(let i = 0; i < pronoun.length; i++) {
-    
-    
-    for(let j = 0; j < adj.length; j++) {
-      
-      
-      for(let k = 0; k < noun.length; k++) {
-        
-        
-        let domain = pronoun[i] + adj[j] + noun[k] + ".com" + ".es" + ".net";
-        console.log(domain);
-      }
-    }
-  }
-};
+  const extension = ['.com', '.es', '.net'];
 
+
+  for (let i = 0; i < pronoun.length; i++) {
+
+
+    for (let j = 0; j < adj.length; j++) {
+
+
+      for (let k = 0; k < noun.length; k++) {
+
+        for (let l = 0; l < extension.length; l++) {
+
+
+          let domain = pronoun[i] + adj[j] + noun[k] + extension[l];
+
+          console.log(domain);
+        };
+      };
+    };
+  };
+};
